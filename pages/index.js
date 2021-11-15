21c37ea7729de52af2412603d759b81195c5ef68
Diogo <diogo.mart.ferreira@gmail.com>
2021-11-15 23:12:09 +0000

    fix warning

--- pages/index.js
+++ pages/index.js
@@ -28,7 +28,7 @@ export default function Home() {
         />
         <link rel="manifest" href="/site.webmanifest"></link>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Modak&display=swap"
           rel="stylesheet"
