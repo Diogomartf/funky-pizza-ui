814194c114d765e9619a9df4ab9eea1449d94f41
Diogo <diogo.mart.ferreira@gmail.com>
2021-11-23 00:50:57 +0000

    Add next image - Update pizza images to bigger resolution - Update team to fit page and use next image

--- pages/components/PizzaCard.js
+++ pages/components/PizzaCard.js
@@ -1,8 +1,15 @@
+import Image from "next/image";
+
 const PizzaCard = ({ color, index }) => {
   return (
     <div className="flex items-center justify-center w-64 h-64 bg-white rounded-lg min-w-[256px]">
       <div className={`w-56 h-56 bg-pastel-${color} rounded`}>
-        <img src={`/pizzas/funky-pizza-${index}.png`} alt="funky pizza" />
+        <Image
+          src={`/pizzas/funky-pizza-${index}.png`}
+          alt="funky pizza"
+          width={500}
+          height={500}
+        />
       </div>
     </div>
   );
--- pages/components/Team.js
+++ pages/components/Team.js
@@ -1,22 +1,33 @@
+import Image from "next/image";
 import team from "../data/team.json";
 
 const Team = () => (
-  <div className="flex -mr-5 space-x-4 overflow-x-auto my-28 md:mr-0">
+  <div className="flex justify-between -mr-5 space-x-5 overflow-x-auto my-28 md:mr-0">
     {team.map((member) => (
       <div className="flex flex-col items-center" key={member.name}>
         <div className="relative flex mb-3">
-          <div className="z-10 flex items-center justify-center w-32 h-32 border-4 rounded-full border-beige bg-tomato">
-            <img src={`/team/${member.name}.webp`} alt="memoji" />
+          <div className="z-10 flex items-center justify-center border-4 rounded-full w-28 h-28 border-beige bg-tomato">
+            <Image
+              src={`/team/${member.name}.webp`}
+              alt={`${member.name} memoji`}
+              width={200}
+              height={200}
+            />
           </div>
           <div
-            className={`w-32 h-32 -ml-12 border-4 rounded-full border-beige bg-pastel-${member.color}`}
+            className={`w-28 h-28 -ml-12 border-4 rounded-full border-beige bg-pastel-${member.color}`}
           >
-            <img src={`/team/${member.name}-pizza.png`} alt="funky pizza" />
+            <Image
+              src={`/team/${member.name}-pizza.png`}
+              alt={`${member.name} funky pizza`}
+              width={200}
+              height={200}
+            />
           </div>
         </div>
         <a
           href={`https://twitter.com/${member.handler}`}
-          className="mb-2 text-2xl font-semibold text-tomato hover:text-tomato/80"
+          className="mb-2 text-2xl font-semibold text-tomato hover:text-tomato/60"
         >
           @{member.handler}
         </a>
