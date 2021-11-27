77c91db907acbe101fd6d4f41c35d6d953c56f3b
Diogo <diogo.mart.ferreira@gmail.com>
2021-11-27 22:21:25 +0000

    Add full url to og and twitter image

--- pages/components/MetaTags.js
+++ pages/components/MetaTags.js
@@ -12,22 +12,34 @@ const MetaTags = () => (
     {/* <!-- Open Graph / Facebook --> */}
     <meta property="og:type" content="website" />
     <meta property="og:url" content="https://www.funky.pizza/" />
-    <meta property="og:title" content="Funky Pizza" />
+    <meta
+      property="og:title"
+      content="Funky Pizza | The best pizza on the blockchain"
+    />
     <meta
       property="og:description"
       content="2205 tasty collectible NFTs for every pizza lover."
     />
-    <meta property="og:image" content="/funky-cover.jpg" />
+    <meta
+      property="og:image"
+      content="https://www.funky.pizza/funky-cover.jpg"
+    />
 
     {/* <!-- Twitter --> */}
     <meta property="twitter:card" content="summary_large_image" />
     <meta property="twitter:url" content="https://www.funky.pizza/" />
-    <meta property="twitter:title" content="Funky Pizza" />
+    <meta
+      property="twitter:title"
+      content="Funky Pizza | The best pizza on the blockchain"
+    />
     <meta
       property="twitter:description"
       content="2205 tasty collectible NFTs for every pizza lover."
     />
-    <meta property="twitter:image" content="/funky-cover.jpg"></meta>
+    <meta
+      property="twitter:image"
+      content="https://www.funky.pizza/funky-cover.jpg"
+    ></meta>
   </>
 );
 
