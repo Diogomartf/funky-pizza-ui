b632521f49ca33e020f5eb239cf4e91b77a7414b
Diogo <diogo.mart.ferreira@gmail.com>
2022-05-22 18:21:20 +0100

    Update mainnet

--- pages/_app.js
+++ pages/_app.js
@@ -5,8 +5,8 @@ import "./styles/global.css";
 function MyApp({ Component, pageProps }) {
   return (
     <Provider
-      network={NETWORKS.rinkeby}
-      rpcUrl={`https://rinkeby.infura.io/v3/${process.env.NEXT_PUBLIC_INFURIA_KEY}`}
+      network={NETWORKS.mainnet}
+      rpcUrl={`https://mainnet.infura.io/v3/${process.env.NEXT_PUBLIC_INFURIA_KEY}`}
     >
       <div className="relative font-sans bg-beige">
         <div className="max-w-[70rem] px-5 py-1 mx-auto">
