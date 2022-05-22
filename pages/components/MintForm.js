34a7ab640605b2b296d66e35062a4e47a046bacc
Berteotti <berteotti2@gmail.com>
2022-05-22 12:50:48 +0100

    Improve mint

--- pages/components/MintForm.js
+++ pages/components/MintForm.js
@@ -60,46 +60,56 @@ export default function MintForm({
   increment,
   decrement,
   mintAmount,
+  isPaused,
+  isFetchingData,
 }) {
   const { connected, connectWallet, correctNetwork } = useWallet();
 
   return (
     <>
       <div className="flex items-center justify-between space-x-3 md:space-x-5">
-        <QuantityInput
-          increment={increment}
-          decrement={decrement}
-          mintAmount={mintAmount}
-        />
-        <button
-          onClick={connected ? mintNFT : connectWallet}
-          className="flex px-6 py-4 text-white uppercase rounded-full cursor-pointer bg-gradient-to-r from-orangy to-orangeCrust hover:drop-shadow-lg hover:duration-200 h-fit"
-          disabled={!correctNetwork}
-        >
-          {loading && (
-            <svg
-              className="w-5 h-5 text-white animate-spin"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
+        {isFetchingData || isPaused ? (
+          <div className="w-full flex justify-center px-6 py-4 text-white uppercase rounded-full cursor-pointer bg-gradient-to-r from-orangy to-orangeCrust hover:drop-shadow-lg hover:duration-200 h-fit">
+            {isFetchingData ? "Preparing ingredients..." : "Mint Paused"}
+          </div>
+        ) : (
+          <>
+            <QuantityInput
+              increment={increment}
+              decrement={decrement}
+              mintAmount={mintAmount}
+            />
+            <button
+              onClick={!isPaused && (connected ? mintNFT : connectWallet)}
+              className="flex px-6 py-4 text-white uppercase rounded-full cursor-pointer bg-gradient-to-r from-orangy to-orangeCrust hover:drop-shadow-lg hover:duration-200 h-fit"
+              disabled={!correctNetwork || isPaused}
             >
-              <circle
-                className="opacity-25"
-                cx="12"
-                cy="12"
-                r="10"
-                stroke="currentColor"
-                strokeWidth="4"
-              ></circle>
-              <path
-                className="opacity-75"
-                fill="currentColor"
-                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-              ></path>
-            </svg>
-          )}
-          {!loading && "Mint"}
-        </button>
+              {loading && (
+                <svg
+                  className="w-5 h-5 text-white animate-spin"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <circle
+                    className="opacity-25"
+                    cx="12"
+                    cy="12"
+                    r="10"
+                    stroke="currentColor"
+                    strokeWidth="4"
+                  ></circle>
+                  <path
+                    className="opacity-75"
+                    fill="currentColor"
+                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                  ></path>
+                </svg>
+              )}
+              {!loading && "Mint"}
+            </button>
+          </>
+        )}
       </div>
       {error && (
         <div className="px-2 py-1 mx-auto text-xs text-red-700 bg-red-100 rounded w-fit">
