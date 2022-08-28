4112b73d0c89e39cde7172d48076e356bd141633
Diogo <diogomf@hey.com>
2022-08-28 16:09:00 +0100

    add address and etherscan link to footer

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -6,8 +6,20 @@ const Footer = () => {
         alt="Funky Pizza logo"
         className="w-40 md:w-56 lg:w-64"
       />
-      <div className="text-sm font-bold text-right text-darkRed">
-        All rights reserved @ Funky Pizza — {new Date().getFullYear()}
+      <div className="space-y-2">
+        <p className="font-mono text-xs text-gray-600">
+          contract address:{" "}
+          <a
+            href="https://etherscan.io/address/0xF71364a7939ff3312363a12daB7cd405cbD31659"
+            className="hover:underline"
+            target="_blank"
+          >
+            {process.env.NEXT_PUBLIC_FUNKY_PIZZA_MAINNET_ADDRESS}
+          </a>
+        </p>
+        <p className="text-sm font-bold text-right text-darkRed">
+          All rights reserved @ Funky Pizza — {new Date().getFullYear()}
+        </p>
       </div>
     </div>
   );
