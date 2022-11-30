466b9b73a3c85487a43030adea7bbdf119e7eee4
Diogo <diogomf@hey.com>
2022-11-30 15:34:43 -0500

    Fix footer

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -1,14 +1,14 @@
 const Footer = () => {
   return (
-    <div className="flex items-end justify-between pt-48 my-8 space-x-6">
+    <div className="flex flex-col pt-48 my-8 space-y-6 md:space-x-6 md:items-end md:justify-between md:flex-row">
       <img
         src="logo.svg"
         alt="Funky Pizza logo"
         className="w-40 md:w-56 lg:w-64"
       />
       <div className="space-y-2">
-        <p className="font-mono text-xs text-gray-600">
-          contract address:{" "}
+        <p className="font-mono text-xs text-gray-500">
+          contract address{" "}
           <a
             href="https://etherscan.io/address/0xF71364a7939ff3312363a12daB7cd405cbD31659"
             className="hover:underline"
